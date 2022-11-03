3fd816838846bbc04ab96c935ee1627183e606ba
lannyalfiani <lannyalfiani13@gmail.com>
2022-11-04 00:45:14 +0700

    setup server

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-
 // if (process.env.NODE_ENV !== 'production') {
 //     require('dotenv').config();
 //     }
-    
-  const express = require("express");
-  const cors = require("cors");
-  const router = require("./routes");
-  const errorHandler = require("./middleware/errorHandler");
-  const app = express();
-  const port = process.env.PORT || 3000;
-  
-  app.use(cors());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  
-//   app.use("/", router)
-//   app.use(errorHandler)
-  
-  app.listen(port, () =>
-    console.log(`App listening on port http://localhost:${port}!`)
-  );
+
+const express = require("express");
+const cors = require("cors");
+const router = require("./routes");
+const errorHandler = require("./middlewares/errorHandlers");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(errorHandler)
+
+
+app.use(`/`, router)
+
+app.listen(port, () =>
+  console.log(`RUMAH SANDAR listening on port http://localhost:${port}!`)
+);
---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+class userController {
+
+  static async registerUser(req, res, next) {
+    try {
+
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  static async loginUser(req, res, next) {
+    try {
+
+    } catch (err) {
+      next(err)
+    }
+  }
+
+}
+
+
+module.exports = userController;
