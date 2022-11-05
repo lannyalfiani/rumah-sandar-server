c1f4b312af8f8d3e4e144d1b4d2a3eea00b831ab
lannyalfiani <lannyalfiani13@gmail.com>
2022-11-05 14:54:13 +0700

    deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-console.log(process.env);
+// console.log(process.env);
 
 const express = require("express");
 const cors = require("cors");
