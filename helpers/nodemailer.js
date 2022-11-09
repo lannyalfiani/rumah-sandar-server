561ac394d3ca5b19a008b0587eab3a1102cadd3a
RizkyFadhilla <rizkyfadhilla123@gmail.com>
2022-11-10 04:25:58 +0700

    add template html to nodemailer register volunteer

--- helpers/nodemailer.js
+++ helpers/nodemailer.js
@@ -1,14 +1,21 @@
 const nodemailer = require("nodemailer");
+const smtpTransport = require("nodemailer-smtp-transport");
+const handlebars = require("handlebars");
+const { promisify } = require("util");
+const path = require('path')
+const fs = require("fs");
 
 async function nodeMailer(email, subject, additionalData) {
   try {
-    let transporter = nodemailer.createTransport({
-      service: "gmail", // true for 465, false for other ports
-      auth: {
-        user: process.env.EMAIL, // generated ethereal user
-        pass: process.env.PASSWORD, // generated ethereal password
-      },
-    });
+    let transporter = nodemailer.createTransport(
+      smtpTransport({
+        service: "gmail", // true for 465, false for other ports
+        auth: {
+          user: process.env.EMAIL, // generated ethereal user
+          pass: process.env.PASSWORD, // generated ethereal password
+        },
+      })
+    );
 
     //! VERIFIED
     if (subject === "Verifikasi Reralawan") {
@@ -38,18 +45,23 @@ async function nodeMailer(email, subject, additionalData) {
 
       //! REGISTRATION - VOLUNTEER
     } else if (subject === "Pendaftaran Kakak Ajar") {
-      await transporter.sendMail({
+      const filePath = path.join(
+        __dirname,
+        "../assets/email-kakak-registrasi.html"
+      );
+      const source = fs.readFileSync(filePath, "utf-8").toString();
+      const template = handlebars.compile(source);
+      // const replacements = {
+      //   username: "Darth Vader",
+      // };
+      const htmlToSend = template();
+      let response = await transporter.sendMail({
         from: process.env.EMAIL, // sender address
         to: email, // list of receivers
         subject: "Pendaftaran akun Kakak Ajar Rumah Sandar berhasil!", // Subject line
-        html: `<div>
-      <h1>${additionalData}, terima kasih telah mendaftar jadi Kakak Ajar di Rumah Sandar!</h1>
-      <p>Tim kami akan segera melakukan verifikasi</p>
-      <p>apabila anda sudah terverifikasi, maka kami akan mengirimkan email bahwa telah terverikasi </p>
-      <p>apabila memiliki pertanyaan silahkan hubungi ke email resmi rumah sandar yakni di : rumahsandarhacktiv@gmail.com</p>
-  </div>`,
+        html: htmlToSend,
       });
-
+      console.log(response);
       //! Schedule - ORPHAN
     } else if (subject === "Jadwal Adik Ajar") {
       await transporter.sendMail({
