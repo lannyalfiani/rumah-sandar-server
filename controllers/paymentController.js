4936ada1f910b27d4ff183c1d4501cf09f1b67bc
RizkyFadhilla <rizkyfadhilla123@gmail.com>
2022-11-10 18:39:45 +0700

    order donation

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,26 +1,24 @@
 const { createInvoice } = require("../helpers/createInvoice.js");
-const { Donation } = require("../models")
+const { Donation } = require("../models");
 class paymentController {
-
   static async acceptCallback(req, res, next) {
     try {
-      let ourCallbackToken = process.env.XENDITCALLBACKTOKEN
-      if (ourCallbackToken === req.headers['x-callback-token']) {
-
-        let callback = req.body
-        let invoiceStatus = callback.status
+      let ourCallbackToken = process.env.XENDITCALLBACKTOKEN;
+      if (ourCallbackToken === req.headers["x-callback-token"]) {
+        let callback = req.body;
+        let invoiceStatus = callback.status;
 
         if (invoiceStatus === `PAID`) {
-          createInvoice(req.body)
-          return res.status(200).json({ message: `Callback received!` })
+          createInvoice(req.body);
+          return res.status(200).json({ message: `Callback received!` });
         } else {
-          throw { name: `INVOICE_NOT_PAID` }
+          throw { name: `INVOICE_NOT_PAID` };
         }
       } else {
-        throw { name: `NOT_FROM_XENDIT` }
+        throw { name: `NOT_FROM_XENDIT` };
       }
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
 
@@ -28,15 +26,15 @@ class paymentController {
     try {
       let data = await Donation.findAll({
         attributes: {
-          exclude: [`createdAt`, `updatedAt`]
-        }
-      })
-      res.status(200).json(data)
+          exclude: [`createdAt`, `updatedAt`],
+        },
+        order: [["id", "ASC"]],
+      });
+      res.status(200).json(data);
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
-
 }
 
 module.exports = paymentController;
